85ac180ce42cb7ae8dca8e64b6cb34a7e2cc41fa
bsbds <69835502+bsbds@users.noreply.github.com>
2024-12-24 14:43:19 +0800

    test: add tests for ring buffer
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

---
+++ rust-driver/src/ring/test.rs
@@ -0,0 +1,53 @@
+use std::iter;
+
+use crate::mem::{page::ConscMem, slot_alloc::SlotAlloc};
+
+use super::*;
+
+#[derive(Default, Clone, Copy)]
+struct TestDesc {
+    inner: [u8; 32],
+}
+
+impl TestDesc {
+    fn new_valid() -> Self {
+        Self { inner: [1; 32] }
+    }
+}
+
+impl Descriptor for TestDesc {
+    fn try_consume(&mut self) -> bool {
+        let valid = self.inner[0] == 1;
+        self.inner[0] = 0;
+        valid
+    }
+
+    fn size() -> usize {
+        32
+    }
+}
+
+#[test]
+fn ring_produce_consume_is_ok() {
+    let slot = vec![TestDesc::default(); 128];
+    let ring_ctx = RingCtx::new(Dummy);
+    let mut ring = Ring::<_, _, TestDesc>::new(ring_ctx, slot).unwrap();
+    let round = 10;
+    for _ in 0..round {
+        for i in 0..128 {
+            ring.produce(iter::once(TestDesc::new_valid())).unwrap();
+        }
+        assert!(ring.produce(iter::once(TestDesc::new_valid())).is_err());
+        for i in 0..128 {
+            assert!(ring.try_consume().is_some());
+        }
+        assert!(ring.try_consume().is_none());
+    }
+}
+
+#[test]
+fn build_ring_buffer_should_reject_insufficient_buf_size() {
+    let slot = vec![TestDesc::default(); 127];
+    let ring_ctx = RingCtx::new(Dummy);
+    assert!(Ring::<_, _, TestDesc>::new(ring_ctx, slot).is_none());
+}
