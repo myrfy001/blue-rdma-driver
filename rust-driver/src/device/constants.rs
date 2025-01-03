d0cbeaa6be19814e9a04e7b0582b59b334307d78
bsbds <69835502+bsbds@users.noreply.github.com>
2025-01-03 10:44:56 +0800

    feat: add simple nic csr addresses
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

--- rust-driver/src/device/constants.rs
+++ rust-driver/src/device/constants.rs
@@ -5,28 +5,31 @@ enum CsrIndex {
     Tail = 0x3,
 }
 
+enum BlockStart {
+    Qp = 0x00,
+    CmdQ = 0x40,
+    SimpleNic = 0x50,
+}
+
 #[allow(clippy::as_conversions, clippy::arithmetic_side_effects)]
 const fn generate_csr_addr(
-    is_cmdq: bool,
+    start: BlockStart,
     queue_index: usize,
     is_2h: bool,
     reg_index: CsrIndex,
 ) -> usize {
-    let t = if is_cmdq {
-        0x40 + (if is_2h { 4 } else { 0 }) + (reg_index as usize)
-    } else {
-        (queue_index * 0x10) + (if is_2h { 4 } else { 0 }) + (reg_index as usize)
-    };
+    let t =
+        start as usize + (if is_2h { 4 } else { 0 }) + (reg_index as usize) + queue_index * 0x10;
     t << 2
 }
 
 macro_rules! generate_qp_array {
     ($is_recv:expr, $reg_index:expr) => {
         [
-            generate_csr_addr(false, 0, $is_recv, $reg_index),
-            generate_csr_addr(false, 1, $is_recv, $reg_index),
-            generate_csr_addr(false, 2, $is_recv, $reg_index),
-            generate_csr_addr(false, 3, $is_recv, $reg_index),
+            generate_csr_addr(BlockStart::Qp, 0, $is_recv, $reg_index),
+            generate_csr_addr(BlockStart::Qp, 1, $is_recv, $reg_index),
+            generate_csr_addr(BlockStart::Qp, 2, $is_recv, $reg_index),
+            generate_csr_addr(BlockStart::Qp, 3, $is_recv, $reg_index),
         ]
     };
 }
@@ -47,18 +50,35 @@ pub(super) const QP_RECV_HEAD: [usize; NUM_QPS] = generate_qp_array!(true, CsrIn
 pub(super) const QP_RECV_TAIL: [usize; NUM_QPS] = generate_qp_array!(true, CsrIndex::Tail);
 
 pub(super) const CSR_ADDR_CMD_REQ_QUEUE_ADDR_LOW: usize =
-    generate_csr_addr(true, 0, false, CsrIndex::BaseAddrLow);
+    generate_csr_addr(BlockStart::CmdQ, 0, false, CsrIndex::BaseAddrLow);
 pub(super) const CSR_ADDR_CMD_REQ_QUEUE_ADDR_HIGH: usize =
-    generate_csr_addr(true, 0, false, CsrIndex::BaseAddrHigh);
+    generate_csr_addr(BlockStart::CmdQ, 0, false, CsrIndex::BaseAddrHigh);
 pub(super) const CSR_ADDR_CMD_REQ_QUEUE_HEAD: usize =
-    generate_csr_addr(true, 0, false, CsrIndex::Head);
+    generate_csr_addr(BlockStart::CmdQ, 0, false, CsrIndex::Head);
 pub(super) const CSR_ADDR_CMD_REQ_QUEUE_TAIL: usize =
-    generate_csr_addr(true, 0, false, CsrIndex::Tail);
+    generate_csr_addr(BlockStart::CmdQ, 0, false, CsrIndex::Tail);
 pub(super) const CSR_ADDR_CMD_RESP_QUEUE_ADDR_LOW: usize =
-    generate_csr_addr(true, 0, true, CsrIndex::BaseAddrLow);
+    generate_csr_addr(BlockStart::CmdQ, 0, true, CsrIndex::BaseAddrLow);
 pub(super) const CSR_ADDR_CMD_RESP_QUEUE_ADDR_HIGH: usize =
-    generate_csr_addr(true, 0, true, CsrIndex::BaseAddrHigh);
+    generate_csr_addr(BlockStart::CmdQ, 0, true, CsrIndex::BaseAddrHigh);
 pub(super) const CSR_ADDR_CMD_RESP_QUEUE_HEAD: usize =
-    generate_csr_addr(true, 0, true, CsrIndex::Head);
+    generate_csr_addr(BlockStart::CmdQ, 0, true, CsrIndex::Head);
 pub(super) const CSR_ADDR_CMD_RESP_QUEUE_TAIL: usize =
-    generate_csr_addr(true, 0, true, CsrIndex::Tail);
+    generate_csr_addr(BlockStart::CmdQ, 0, true, CsrIndex::Tail);
+
+pub(super) const CSR_ADDR_OFFSET_SIMPLE_NIC_TX_Q_RINGBUF_BASE_ADDR_LOW: usize =
+    generate_csr_addr(BlockStart::SimpleNic, 0, false, CsrIndex::BaseAddrLow);
+pub(super) const CSR_ADDR_OFFSET_SIMPLE_NIC_TX_Q_RINGBUF_BASE_ADDR_HIGH: usize =
+    generate_csr_addr(BlockStart::SimpleNic, 0, false, CsrIndex::BaseAddrHigh);
+pub(super) const CSR_ADDR_OFFSET_SIMPLE_NIC_TX_Q_RINGBUF_HEAD: usize =
+    generate_csr_addr(BlockStart::SimpleNic, 0, false, CsrIndex::Head);
+pub(super) const CSR_ADDR_OFFSET_SIMPLE_NIC_TX_Q_RINGBUF_TAIL: usize =
+    generate_csr_addr(BlockStart::SimpleNic, 0, false, CsrIndex::Tail);
+pub(super) const CSR_ADDR_OFFSET_SIMPLE_NIC_RX_Q_RINGBUF_BASE_ADDR_LOW: usize =
+    generate_csr_addr(BlockStart::SimpleNic, 0, true, CsrIndex::BaseAddrLow);
+pub(super) const CSR_ADDR_OFFSET_SIMPLE_NIC_RX_Q_RINGBUF_BASE_ADDR_HIGH: usize =
+    generate_csr_addr(BlockStart::SimpleNic, 0, true, CsrIndex::BaseAddrHigh);
+pub(super) const CSR_ADDR_OFFSET_SIMPLE_NIC_RX_Q_RINGBUF_HEAD: usize =
+    generate_csr_addr(BlockStart::SimpleNic, 0, true, CsrIndex::Head);
+pub(super) const CSR_ADDR_OFFSET_SIMPLE_NIC_RX_Q_RINGBUF_TAIL: usize =
+    generate_csr_addr(BlockStart::SimpleNic, 0, true, CsrIndex::Tail);
