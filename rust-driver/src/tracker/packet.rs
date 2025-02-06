b71bc8aa73c44be49f38f829813b02e7d1d87c9d
bsbds <69835502+bsbds@users.noreply.github.com>
2025-02-06 22:37:54 +0800

    refactor: packet tracker
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

--- rust-driver/src/tracker/packet.rs
+++ rust-driver/src/tracker/packet.rs
@@ -9,18 +9,12 @@ use crate::constants::{MAX_PSN_WINDOW, PSN_MASK};
 
 #[derive(Default, Debug, Clone)]
 pub(crate) struct PacketTracker {
-    base_psn: Arc<AtomicU32>,
+    base_psn: u32,
     inner: BitVec,
 }
 
+#[allow(clippy::cast_possible_wrap, clippy::cast_sign_loss)] // won't wrap since we only use 24bits of the u32
 impl PacketTracker {
-    pub(crate) fn new_with_default_base() -> Self {
-        Self {
-            base_psn: Arc::new(0.into()),
-            inner: BitVec::default(),
-        }
-    }
-
     #[allow(clippy::as_conversions)] // u32 to usize
     /// Acknowledges a range of PSNs starting from `base_psn` using a bitmap.
     ///
@@ -28,12 +22,8 @@ impl PacketTracker {
     ///
     /// Returns `Some(PSN)` if the left edge of the PSN window is advanced, where the
     /// returned `PSN` is the new base PSN value after the advance.
-    #[allow(clippy::cast_possible_wrap, clippy::cast_sign_loss)]
     pub(crate) fn ack_range(&mut self, mut now_psn: u32, mut bitmap: u128) -> Option<u32> {
-        // won't wrap since we only use 24bits of the u32
-        let now_psn_int = now_psn as i32;
-        let base_psn_int = self.base_psn() as i32;
-        let rstart = now_psn_int - base_psn_int;
+        let rstart = self.rstart(now_psn);
         let rend = rstart + 128;
         if let Ok(x) = usize::try_from(rend) {
             if x > self.inner.len() {
@@ -58,16 +48,11 @@ impl PacketTracker {
     /// Returns `Some(PSN)` if the left edge of the PSN window is advanced, where the
     /// returned `PSN` is the new base PSN value after the advance.
     pub(crate) fn ack_one(&mut self, psn: u32) -> Option<u32> {
-        let rstart = (psn.wrapping_sub(self.base_psn()) & PSN_MASK) as usize;
-        let rend = rstart.wrapping_add(1);
-        if rend > MAX_PSN_WINDOW {
-            return None;
-        }
-        if rend > self.inner.len() {
-            self.inner.resize(rend, false);
+        let rstart: usize = usize::try_from(self.rstart(psn)).ok()?;
+        if rstart >= self.inner.len() {
+            self.inner.resize(rstart + 1, false);
         }
         self.inner.set(rstart, true);
-
         self.try_advance()
     }
 
@@ -79,13 +64,9 @@ impl PacketTracker {
     /// Returns `Some(PSN)` if the left edge of the PSN window is advanced, where the
     /// returned `PSN` is the new base PSN value after the advance.
     pub(crate) fn ack_before(&mut self, psn: u32) -> Option<u32> {
-        let rstart = (psn.wrapping_sub(self.base_psn()) & PSN_MASK) as usize;
-        let rend = rstart.wrapping_add(1);
-        if rend > MAX_PSN_WINDOW {
-            return None;
-        }
-        self.set_base_psn(psn);
-        if rend > self.inner.len() {
+        let rstart: usize = usize::try_from(self.rstart(psn)).ok()?;
+        self.base_psn = psn;
+        if rstart >= self.inner.len() {
             self.inner.fill(false);
         } else {
             self.inner.shift_left(rstart);
@@ -96,18 +77,18 @@ impl PacketTracker {
     #[allow(clippy::as_conversions)] // u32 to usize
     /// Returns `true` if all PSNs up to and including the given PSN have been acknowledged.
     pub(crate) fn all_acked(&self, psn_to: u32) -> bool {
-        let x = self.base_psn().wrapping_sub(psn_to) & PSN_MASK;
+        let x = self.base_psn.wrapping_sub(psn_to) & PSN_MASK;
         x > 0 && (x as usize) < MAX_PSN_WINDOW
     }
 
-    /// Returns the current base PSN
     pub(crate) fn base_psn(&self) -> u32 {
-        self.base_psn.load(Ordering::Acquire)
+        self.base_psn
     }
 
-    /// Returns the current base PSN
-    fn set_base_psn(&self, value: u32) {
-        self.base_psn.store(value, Ordering::Release);
+    fn rstart(&self, psn: u32) -> i32 {
+        let now_psn_int = psn as i32;
+        let base_psn_int = self.base_psn as i32;
+        now_psn_int - base_psn_int
     }
 
     /// Try to advance the base PSN to the next unacknowledged PSN.
@@ -123,9 +104,8 @@ impl PacketTracker {
             return None;
         }
         self.inner.shift_left(pos);
-        let mut psn = self.base_psn();
-        psn = psn.wrapping_add(pos as u32) & PSN_MASK;
-        self.set_base_psn(psn);
+        let mut psn = self.base_psn;
+        self.base_psn = self.base_psn.wrapping_add(pos as u32) & PSN_MASK;
         Some(psn)
     }
 }
@@ -146,26 +126,26 @@ mod tests {
     fn test_ack_range() {
         let mut tracker = PacketTracker::default();
         tracker.ack_range(0, 0b11); // PSN 0 and 1
-        assert_eq!(tracker.base_psn(), 2);
+        assert_eq!(tracker.base_psn, 2);
         assert!(tracker.inner.not_all());
 
         let mut tracker = PacketTracker {
-            base_psn: Arc::new(5.into()),
+            base_psn: 5,
             ..Default::default()
         };
         tracker.ack_range(5, 0b11);
-        assert_eq!(tracker.base_psn(), 7);
+        assert_eq!(tracker.base_psn, 7);
         assert!(tracker.inner.not_all());
 
         let mut tracker = PacketTracker {
-            base_psn: Arc::new(10.into()),
+            base_psn: 10,
             ..Default::default()
         };
         tracker.ack_range(5, 0b11);
-        assert_eq!(tracker.base_psn(), 10);
-        assert!(tracker.inner.is_empty());
+        assert_eq!(tracker.base_psn, 10);
+        assert!(tracker.inner.not_all());
         tracker.ack_range(20, 0b11);
-        assert_eq!(tracker.base_psn(), 10);
+        assert_eq!(tracker.base_psn, 10);
         assert!(tracker.inner[10]);
         assert!(tracker.inner[11]);
     }
@@ -173,7 +153,7 @@ mod tests {
     #[test]
     fn test_all_acked() {
         let tracker = PacketTracker {
-            base_psn: Arc::new(10.into()),
+            base_psn: 10,
             ..Default::default()
         };
         assert!(tracker.all_acked(9));
@@ -184,7 +164,7 @@ mod tests {
     #[test]
     fn test_wrapping_ack() {
         let mut tracker = PacketTracker {
-            base_psn: Arc::new((PSN_MASK - 1).into()),
+            base_psn: PSN_MASK - 1,
             ..Default::default()
         };
         tracker.ack_range(0, 0b11);
