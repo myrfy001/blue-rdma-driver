68054d6fc65808bde1b512375c18ca59c6655fa3
bsbds <69835502+bsbds@users.noreply.github.com>
2025-04-15 18:08:05 +0800

    chore: add rust toolchain
    Signed-off-by: bsbds <69835502+bsbds@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -4,17 +4,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        kernel = pkgs.linuxPackages_6_12.kernel;
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        kernel = pkgs.linuxPackages_6_6.kernel;
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
       in
       {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
+            rustToolchain
+
             rdma-core
             cmake
             docutils
